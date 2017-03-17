3a47db82c0120e09d8b4c04d5857e3c3fc6397d3
Miguel Muscat <miguelmuscat93@gmail.com>
2017-03-17 15:46:04 +0100

    Fixed code standards

--- src/Exception/EvaluationException.php
+++ src/Exception/EvaluationException.php
@@ -4,6 +4,7 @@ namespace Dhii\Expression\Exception;
 
 use Dhii\Evaluable\EvaluationExceptionInterface;
 use Symfony\Component\Config\Definition\Exception\Exception;
+
 /**
  * An exception thrown during expression evaluation, when a problem is encountered.
  *
